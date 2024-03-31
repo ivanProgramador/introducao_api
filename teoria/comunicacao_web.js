0778666ddf37ac6062a84a33cb0e53684a0191cc
ivan <ivan-guita@hotmail.com>
2024-03-31 20:18:41 -0300

    teoria protocolo http / verbos http final da aula 189

---
+++ teoria/comunicacao_web.js
@@ -0,0 +1,29 @@
+/*
+  HTTP
+
+  Todas as aplicações web usam o protocolo http para se comunicar 
+  o portocolo http torna possivel o acesso a sites/dados que estejam 
+  na web.
+
+  VERBOS HTTP 
+
+  diferenças 
+
+  http  -> protocolo de comunicação direta sem autenticação
+  https -> protocolo de comunicação com mecanicas de autenticação(certificado / login/ senha/criptografia .....)
+           o 's' no final significa secure.
+
+  GET - > requisitar dados de uma rota http ou https                                        suporta -> links
+  POST -> enviar dados para uma rota http ou https                                          suporta -> formularios
+  PUT  -> atualizar totalmente os dados usando uma rota http ou https                       suporta -> formularios
+  PATCH -> atualizar parte de um recurso existente atraves de uma rota http ou https        suporta -> formularios
+  DELETE -> apagar um item atraves de uma rota http ou https                                suporta -> links e formuularios
+
+  
+
+
+
+ 
+ 
+
+*/
