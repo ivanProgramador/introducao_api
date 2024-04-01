b10b7fb8b37b07343e298ca4000e4c2c8fdf0e8d
ivan <ivan-guita@hotmail.com>
2024-03-31 23:11:46 -0300

    teoria status code fim da aula 191

--- teoria/comunicacao_web.js
+++ teoria/comunicacao_web.js
@@ -19,7 +19,19 @@
   PATCH -> atualizar parte de um recurso existente atraves de uma rota http ou https        suporta -> formularios
   DELETE -> apagar um item atraves de uma rota http ou https                                suporta -> links e formuularios
 
+  STATUS CODE 
+
+  Mostra atráves de códigos o status de uma requisição existem muitos status code porém existem alguns principais, nem 
+  todos precisam ser usados dependendo da finalziadade da sua api, o mais basicos e funcionais são 
   
+  200 status ok 
+  404 e quando ele até acessa o servidor mais não encontro o recurso ou rota solicitada  
+  500 erro no servidor 
+  401 erro de autenticação 
+
+  Esses são alguns dos principais mas caso a minha api tenha um caso especifico esse site tem uma lista 
+  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
 
 
 
